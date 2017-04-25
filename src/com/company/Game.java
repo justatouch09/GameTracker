3d7f2bb63eaca70c708001ee454b88952fbd98e5
Jarad Touchberry <you@example.com>
2017-04-25 14:00:38 -0400

    cool project

---
+++ src/com/company/Game.java
@@ -0,0 +1,18 @@
+package com.company;
+
+/**
+ * Created by jaradtouchberry on 4/25/17.
+ */
+public class Game {
+    String name;
+    String genre;
+    String platform;
+    int releaseYear;
+
+    public Game(String name, String genre, String platform, int releaseYear) {
+        this.name = name;
+        this.genre = genre;
+        this.platform = platform;
+        this.releaseYear = releaseYear;
+    }
+}
